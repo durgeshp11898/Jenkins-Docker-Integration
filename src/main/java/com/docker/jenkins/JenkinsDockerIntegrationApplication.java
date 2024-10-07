18c364e9f052b83a423242bce644285c783b4671
Durgesh Pawar <durgeshpawar077@gmail.com>
2024-10-07 21:04:55 +0530

    Your commit message

--- src/main/java/com/docker/jenkins/JenkinsDockerIntegrationApplication.java
+++ src/main/java/com/docker/jenkins/JenkinsDockerIntegrationApplication.java
@@ -2,12 +2,24 @@ package com.docker.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class JenkinsDockerIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerIntegrationApplication.class, args);
 	}
+
+
+	@GetMapping("/path")
+	public String getMethodName() {
+		return "Durgesh:";
+	}
+
 
 }
